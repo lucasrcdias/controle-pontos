c2c2ba9c23da2e76f114a3375bf2aab97fdcfc30
Lucas Ramos <lucasrcdias@gmail.com>
2016-03-01 20:49:23 -0300

    Autenticação de usuario e vinculação de usuario com a empresa

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,9 +1,16 @@
 class CompaniesController < ApplicationController
   layout "application"
 
+  before_action :authenticate_company_user!
+
   expose(:company, attributes: :company_params)
 
+  def new
+    redirect_to dashboard_index_path and return if current_company_user.company.present?
+  end
+
   def create
+    company.company_user = current_company_user
     company.save
     respond_with company, location: root_path
   end
