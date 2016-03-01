bcd4962b71a890db657e47855900aef922d9c47a
Lucas Ramos <lucasrcdias@gmail.com>
2016-03-01 12:11:53 -0300

    Rota para criação de empresas, pós registro e index do dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: "companies#new"
 
-  resources :companies, only: :create
+  devise_for :company_users, controllers: { registrations: 'company_users/registrations' }
+  devise_scope :company_user do
+    get 'sucesso',   to: 'company_users/registrations#success', as: :success_sign_up
+    get 'dashboard', to: 'dashboard#index',                     as: :dashboard_index
+  end
+
+  resources :companies, only: [:new, :create]
 end
