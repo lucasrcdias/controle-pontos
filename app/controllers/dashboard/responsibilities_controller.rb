522d74faec766fbcb3b56ebd64b41eab7534f180
Lucas Ramos <lucasrcdias@gmail.com>
2016-03-02 00:03:12 -0300

    Controllers do dashboard

---
+++ app/controllers/dashboard/responsibilities_controller.rb
@@ -0,0 +1,24 @@
+class Dashboard::ResponsibilitiesController < Dashboard::BaseController
+  expose(:responsibilities) { current_company_user.company.responsibilities }
+  expose(:responsibility, attributes: :responsibility_params)
+
+  def create
+    if responsibility.save
+      respond_with responsibility, location: [:dashboard, :responsibilities]
+    else
+      respond_with responsibility, location: new_dashboard_responsibility_path
+    end
+  end
+
+  def destroy
+    if responsibility.destroy
+      respond_with responsibility, location: dashboard_responsibilities_path
+    end
+  end
+
+  protected
+
+  def responsibility_params
+    params.require(:responsibility).permit!
+  end
+end
