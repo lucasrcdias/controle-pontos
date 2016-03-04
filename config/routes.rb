890ff4a3c48f49567306aa24e26ca41cad1881e4
Lucas Ramos <lucasrcdias@gmail.com>
2016-03-04 10:06:42 -0300

    Corrigindo rotas do dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   devise_for :company_users, controllers: { registrations: 'company_users/registrations' }
   devise_scope :company_user do
     get 'sucesso',   to: 'company_users/registrations#success', as: :success_sign_up
-    get 'dashboard', to: 'dashboard#index',                     as: :dashboard_index
   end
 
   namespace :dashboard do
-    get 'dashboard', to: 'dashboard#index', as: :dashboard_index
+    root to: 'dashboard#index'
     resources :responsibilities, except: :show, path: 'cargos'
   end
 
