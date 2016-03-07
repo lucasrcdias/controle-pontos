4041c881c267884d0448d3a8b13c31f2d85c5eaa
Lucas Ramos <lucasrcdias@gmail.com>
2016-03-07 09:32:33 -0300

    Corrigindo testes

--- spec/models/period_spec.rb
+++ spec/models/period_spec.rb
@@ -1,13 +1,24 @@
 require 'spec_helper'
 
 describe Period do
+  let(:period) { create(:period) }
+
   describe "validations" do
     it { should validate_presence_of(:start_at) }
     it { should validate_presence_of(:finish_at) }
-    it { should validate_presence_of(:interval) }
+    it { should validate_presence_of(:interval_start) }
+    it { should validate_presence_of(:interval_finish) }
   end
 
   describe "relationships" do
     it { should belong_to(:company) }
+  end
+
+  describe "#interval_duration" do
+    it { expect(period.interval_duration).to eq(1) }
+  end
+
+  describe "#workload" do
+    it { expect(period.workload).to eq(9) }
   end
 end
