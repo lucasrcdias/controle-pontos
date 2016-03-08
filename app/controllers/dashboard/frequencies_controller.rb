77d5998c6737287e4cd1725f0cd1a11b245be842
Lucas Ramos <lucasrcdias@gmail.com>
2016-03-08 11:07:20 -0300

    View e controller de frequencia

---
+++ app/controllers/dashboard/frequencies_controller.rb
@@ -0,0 +1,31 @@
+class Dashboard::FrequenciesController < Dashboard::BaseController
+  expose(:frequencies)
+  expose(:frequency, attributes: :frequency_params)
+
+  def create
+    if frequency.save
+      respond_with frequency, location: [:dashboard, :frequencies]
+    else
+      respond_with frequency, location: new_dashboard_frequency_path
+    end
+  end
+
+  def update
+    if frequency.save
+      respond_with frequency, location: [:dashboard, :frequencies]
+    else
+      respond_with frequency, location: edit_dashboard_frequency_path
+    end
+  end
+
+  def destroy
+    frequency.destroy
+    respond_with frequency, location: [:dashboard, :frequencies]
+  end
+
+  protected
+
+  def frequency_params
+    params.require(:frequency).permit!
+  end
+end
