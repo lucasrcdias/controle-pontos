3895637c4c7b487546a8132cad6e69c4920ecf7e
Lucas Ramos <lucasrcdias@gmail.com>
2016-03-08 11:06:06 -0300

    Renomeando enumeration

---
+++ app/enumerations/week_days.rb
@@ -0,0 +1,21 @@
+class WeekDays < EnumerateIt::Base
+  associate_values(
+    monday: [1, 'Segunda-feira'],
+    tuesday: [2, 'Terça-feira'],
+    wednesday: [3, 'Quarta-feira'],
+    thursday: [4, 'Quinta-feira'],
+    friday: [5, 'Sexta-feira'],
+    saturday: [6, 'Sábado'],
+    sunday: [7, 'Domingo']
+  )
+
+  def self.week_days_from_array(days_list)
+    week_days = []
+
+    days_list.each do |day|
+      week_days << WeekDays.t(day)
+    end
+
+    week_days.to_sentence
+  end
+end
