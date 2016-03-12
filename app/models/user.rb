7fefce355432d16165383539f25853044225b6ce
Lucas Ramos <lucasrcdias@gmail.com>
2016-03-12 12:10:33 -0300

    Criando modelo de use

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
+
+  has_one :manager, dependent: :destroy
+end
