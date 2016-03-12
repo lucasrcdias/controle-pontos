a8ecc5d85ce7115b32432c63a0365294978c5ca3
Lucas Ramos <lucasrcdias@gmail.com>
2016-03-12 15:59:38 -0300

    Testando has_company? e company

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe User do
+  let(:user) { create(:user) }
+  let(:company) { create(:company) }
+
   describe "validations" do
     it { should validate_presence_of(:name) }
   end
@@ -10,8 +13,6 @@ describe User do
   end
 
   describe "#manager?" do
-    let!(:user) { create(:user) }
-
     context "is manager" do
       before { user.update(manager: Manager.new) }
 
@@ -21,5 +22,27 @@ describe User do
     context "isnt manager" do
       it { expect(user.manager?).to be false }
     end
+  end
+
+  describe "#has_company?" do
+    context "when has company" do
+      before { user.update(manager: Manager.new(company: company)) }
+
+      it { expect(user.has_company?).to be true }
+    end
+
+    context "when hasnt company" do
+      it { expect(user.has_company?).to be false }
+    end
+  end
+
+  describe "#company" do
+    before { user.update(manager: Manager.new(company: company)) }
+
+    it { expect(user.company.fantasy_name).not_to be nil }
+    it { expect(user.company.fantasy_name).to eql(company.fantasy_name) }
+    it { expect(user.company.social_reason).to eql(company.social_reason) }
+    it { expect(user.company.cnpj).to eql(company.cnpj) }
+    it { expect(user.company.code).to eql(company.code) }
   end
 end
