b4f77b4c3f6fd41831996eca8d387d85bbd2f4c1
Lucas Ramos <lucasrcdias@gmail.com>
2016-03-12 12:06:10 -0300

    Relacionamento com manager

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,7 @@ class Company < ActiveRecord::Base
   validates :social_reason, :fantasy_name, :cnpj, presence: true
   validates :cnpj, :code, uniqueness: true
 
-  belongs_to :company_user
+  belongs_to :manager
   has_many :roles, dependent: :destroy
   has_many :periods, dependent: :destroy
   has_many :frequencies, dependent: :destroy
