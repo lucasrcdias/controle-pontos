bac67d167fb8504efa8824ce82e57c3bace7da37
Lucas Ramos <lucasrcdias@gmail.com>
2016-03-15 09:08:56 -0300

    Organizando javascript para dashboard

---
+++ app/assets/javascripts/dashboard/sections/workers.js
@@ -0,0 +1,16 @@
+Module('App.DashboardWorkers', function (Workers) {
+  Workers.fn.initialize = function () {
+    this.$workerKind    = $('span.radio input');
+    this.$documentField = $('.js-document-field');
+    this.$workerKinds   = { 0: 'cpf', 1: 'cnpj' };
+
+    this.$workerKind.on('change', $.proxy(this.toggleWorkerKind, this));
+  };
+
+  Workers.fn.toggleWorkerKind = function () {
+    var checked  = this.$workerKind.filter(':checked');
+    var maskType = this.$workerKinds[checked.val()];
+
+    this.$documentField.setMask(maskType);
+  };
+});
