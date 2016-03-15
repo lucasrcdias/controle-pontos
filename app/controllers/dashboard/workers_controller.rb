567f58e143cd3cb030d9e1b307f42a7a3f66aaa1
Lucas Ramos <lucasrcdias@gmail.com>
2016-03-15 09:09:09 -0300

    Criando controller do funcionario

---
+++ app/controllers/dashboard/workers_controller.rb
@@ -0,0 +1,17 @@
+class Dashboard::WorkersController < Dashboard::BaseController
+  expose(:roles)       { current_user.company.roles }
+  expose(:workers)     { current_user.company.workers }
+  expose(:periods)     { current_user.company.periods }
+  expose(:frequencies) { current_user.company.frequencies }
+  expose(:worker, attributes: :worker_params)
+
+  def new
+    worker.user = User.new unless worker.user.present?
+  end
+
+  private
+
+  def worker_params
+    params.require(:worker).permit!
+  end
+end
