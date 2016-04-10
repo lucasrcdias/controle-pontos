8c13ab53c2723cb69d000fd5a48f6e5dec01bd69
Lucas Ramos <lucasrcdias@gmail.com>
2016-04-10 14:04:04 -0300

    Movendo serviço para concern - DRY

--- spec/services/unique_code_generator_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe UniqueCodeGenerator do
-  let(:company) { build(:company) }
-
-  describe "#generate_code" do
-    it { expect(company.code).not_to eql(UniqueCodeGenerator.generate_code(Company)) }
-  end
-end
