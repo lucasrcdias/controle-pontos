1c2c645d63b20a9284d1f69e7e70ee1bfc1d2ade
Lucas Ramos <lucasrcdias@gmail.com>
2016-04-10 14:40:30 -0300

    Corrigindo configurações do mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: Settings.smtp.from, content_type: 'text/html'
 
-  def self.smtp_settings
-    Settings.smtp.settings.symbolize_keys
-  end
-
   layout 'mailer'
 end
