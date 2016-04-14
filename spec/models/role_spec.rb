1426eb9c226910d1f7a7e1d47c793988c84b9aa3
Lucas Ramos <lucasrcdias@gmail.com>
2016-04-14 07:24:52 -0300

    Validando unicidade do cargo

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Role do
   describe 'validations' do
     it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
   end
 
   describe 'relationships' do
