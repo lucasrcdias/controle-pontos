2c16867528374ca46cdf4e0ab806503ce2e08508
Lucas Ramos <lucasrcdias@gmail.com>
2016-04-14 07:28:41 -0300

    Validando unicidade da frequencia

--- app/models/frequency.rb
+++ app/models/frequency.rb
@@ -1,5 +1,6 @@
 class Frequency < ActiveRecord::Base
   validates :days, length: { minimum: 1, message: I18n.t('activerecord.errors.messages.week_days') }
+  validates :days, uniqueness: true
 
   belongs_to :company
 
