399ef11513b1fecdff6135a56eb12c2b53fa2ee3
Lucas Ramos <lucasrcdias@gmail.com>
2016-04-16 18:48:36 -0300

    Utilizando custom validator

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,5 +1,6 @@
 class Point < ActiveRecord::Base
-  validates :latitude, :longitude, :date, :time, presence: true
+  validates :latitude, :longitude, :date, presence: true
+  validates_with PointLimitValidator
 
   belongs_to :worker
 end
