ea6d3f14cf5215d47ffc58c1c1665ccedcb38c71
Lucas Ramos <lucasrcdias@gmail.com>
2016-04-16 18:48:52 -0300

    Removendo teste do time

--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Point do
   describe "validations" do
     it { should validate_presence_of(:date) }
-    it { should validate_presence_of(:time) }
     it { should validate_presence_of(:latitude) }
     it { should validate_presence_of(:longitude) }
   end
