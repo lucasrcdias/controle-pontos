625df3656cbe12e897dd906cc804a038ff77a6f8
Lucas Ramos <lucasrcdias@gmail.com>
2016-04-16 19:12:17 -0300

    Criando scope para pontos do mes

--- app/models/point.rb
+++ app/models/point.rb
@@ -3,4 +3,6 @@ class Point < ActiveRecord::Base
   validates_with PointLimitValidator
 
   belongs_to :worker
+
+  scope :on_month, ->(date) { where(date: date.beginning_of_day..date.end_of_day) }
 end
