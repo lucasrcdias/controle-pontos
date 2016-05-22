80198b5c8d7aba3a77732c75ceac1ffc5fa1ee7c
Lucas Ramos <lucasrcdias@gmail.com>
2016-05-21 22:11:54 -0300

    Define escopo para limitar unicidade

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,6 @@
 class Role < ActiveRecord::Base
   validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: { scope: :company }
 
   belongs_to :company
 end
