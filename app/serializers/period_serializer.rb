fee30193e5829df754ac835309dbe30c16bf57ef
Lucas Ramos <lucasrcdias@gmail.com>
2016-09-23 22:19:08 -0300

    Corrige serializer para retornar inicio e fim das horas extra

--- app/serializers/period_serializer.rb
+++ app/serializers/period_serializer.rb
@@ -1,3 +1,11 @@
 class PeriodSerializer < ActiveModel::Serializer
-  attributes :start_at, :finish_at, :interval_start, :interval_finish
+  attributes :start_at, :finish_at, :interval_start, :interval_finish, :extra_hour_start, :extra_hour_finish
+
+  def extra_hour_start
+    object.finish_at + 1.second
+  end
+
+  def extra_hour_finish
+    extra_hour_start + (object.company.extra_hours_limit.to_i || 2.hours)
+  end
 end
