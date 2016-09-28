31526a27a2a8c633bf44c9904914b1eecbbfdfa7
Lucas Ramos <lucasrcdias@gmail.com>
2016-09-28 20:06:12 -0300

    Altera metodo para incluir classe referente a distancia

--- app/helpers/points_helper.rb
+++ app/helpers/points_helper.rb
@@ -1,12 +1,20 @@
 module PointsHelper
   PointsKind.values.each do |value|
     define_method("#{value}_date") do |date|
-      record = Point.send("#{value}_by_date", date)
-      if record
-        l(record.created_at, format: :short)
+      point   = Point.send("#{value}_by_date", date)
+      content = if point
+        l(point.created_at, format: :short)
       else
         "-"
       end
+
+      content_tag(:td, content, class: range_class(point))
     end
+  end
+
+  def range_class(point)
+    "in-range" if point.present? && point.in_company_range?
+
+    "out-of-range"
   end
 end
