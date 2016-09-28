7d60ecbcb09e14e33935a57cd3e4dd2dbb82005e
Yuri Santos <yurisantos47@gmail.com>
2016-09-28 20:01:30 -0300

    Adiciona total de horas

--- app/helpers/points_helper.rb
+++ app/helpers/points_helper.rb
@@ -1,6 +1,6 @@
 module PointsHelper
   PointsKind.values.each do |value|
-    define_method("#{value}_date") do |date|
+    define_method("#{value}_hour") do |date|
       record = Point.send("#{value}_by_date", date)
       if record
         l(record.created_at, format: :short)
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -19,4 +19,26 @@ class Worker < ActiveRecord::Base
   def has_password?
     user.sign_in_count.zero? && user.reset_password_token.present?
   end
+
+  def hours_per_month(date)
+    points_per_month = points.on_month(date)
+
+    entry_hour          = points_per_month.entry.sum("extract(hour from created_at)")
+    leave_hour          = points_per_month.leave.sum("extract(hour from created_at)")
+    start_interval_hour = points_per_month.start_interval.sum("extract(hour from created_at)")
+    end_interval_hour   = points_per_month.end_interval.sum("extract(hour from created_at)")
+
+    ((leave_hour - entry_hour) - (end_interval_hour - start_interval_hour)).to_i
+  end
+
+  def extra_time_by_month(date)
+    points_per_month = points.on_month(date)
+
+    return 0 if points_per_month.start_extra_time.blank? && points_per_month.leave_extra_time.blank?
+
+    start_extra_time = points_per_month.start_extra_time.sum("extract(hour from created_at)")
+    last_extra_time  = points_per_month.leave_extra_time.sum("extract(hour from created_at)")
+
+    (last_extra_time - start_extra_time).to_i
+  end
 end
