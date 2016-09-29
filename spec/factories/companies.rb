54fe421711a16c6a6d73fc0e518e321dae9dfbd0
Lucas Ramos <lucasrcdias@gmail.com>
2016-09-29 09:21:16 -0300

    Corrige teste de companyg

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :company do
     social_reason { FFaker::NameBR.name }
     fantasy_name  { FFaker::NameBR.name }
+    latitude      { FFaker::Geolocation.lat }
+    longitude     { FFaker::Geolocation.lng }
     cnpj          { '64249243000140' }
     radius        { 50 }
   end
