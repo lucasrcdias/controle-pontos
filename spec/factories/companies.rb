84e0ac7c33889256ce58982da1a94c4a4d0e1cc9
Lucas Ramos <lucasrcdias@gmail.com>
2016-09-29 09:16:02 -0300

    Inclui radius no factory de company

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :company do
-    social_reason FFaker::NameBR.name
-    fantasy_name FFaker::NameBR.name
-    cnpj '64249243000140'
+    social_reason { FFaker::NameBR.name }
+    fantasy_name  { FFaker::NameBR.name }
+    cnpj          { '64249243000140' }
+    radius        { 50 }
   end
 end
