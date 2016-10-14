d4ba6b367d12cfcdd1952cdffdd6427fd73e622a
Lucas Ramos <lucasrcdias@gmail.com>
2016-10-13 22:42:29 -0300

    Inclui redirecionamento para usuário

--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -4,4 +4,9 @@ class Dashboard::BaseController < ApplicationController
   layout 'dashboard'
 
   before_action :authenticate_manager!
+  before_action :verify_company
+
+  def verify_company
+    redirect_to new_company_path unless user_signed_in? && current_user.has_company?
+  end
 end
--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,3 +1,11 @@
 class StaticsController < ApplicationController
   layout 'landing'
+
+  def home
+    if user_signed_in?
+      return redirect_to dashboard_root_path if current_user.manager?
+
+      redirect_to worker_confirmation_path
+    end
+  end
 end
